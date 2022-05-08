68866d40fb4eea8620ea17a1085a6a0d172cc31f
Faruk Aygün <farukaygun1@outlook.com>
2022-05-08 20:04:24 +0300

    first commit

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+/**
+ * ! don't forget to run once for registration after added new commands!
+ */
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v10');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('random').setDescription('Replies with random Wattson text quip!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: 10 }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+	.then(() => console.log("Successfully registered application commands!"))
+	.catch(console.error);
