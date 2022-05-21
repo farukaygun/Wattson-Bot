dbb6d0a1bae3e525bfefa3dcd85feb394e27c12f
Faruk Aygün <farukaygun1@outlook.com>
2022-05-21 15:21:14 +0300

    Wattson shows now how many servers she has joined.

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,13 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		client.user.setActivity(
+			`Wattson is on ${client.guilds.cache.size} Servers.`,
+			{
+				type: 'PLAYING',
+			}
+		);
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
